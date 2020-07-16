f3f552762e81d7dea6416077a7c6a9910b4bcde7
yuansh3354 <65653560+yuansh3354@users.noreply.github.com>
2020-07-16 10:02:49 +0800

    Update R包下载.R

--- R包下载.R
+++ R包下载.R
@@ -1,15 +1,15 @@
-rm(list = ls())   
-options(BioC_mirror="http://mirrors.tuna.tsinghua.edu.cn/bioconductor/")
-options("repos" = c(CRAN="http://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
+# 因为我自己用的是 mac 电脑，所以如果有安装包的话首选直接使用终端+梯子下载各种数据和 R 包
+# 除非代理不能用，不然不使用镜像下载
 
-# https://bioconductor.org/packages/release/bioc/html/GEOquery.html
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-options()$repos
-##  先用这个
+
+##  先用这个，这个是 R 的官方库，可能需要代理才比较好进去
 install.packages("geomnet", repos='https://mran.microsoft.com/snapshot/2019-02-01/')
+
 # 不行用这个
 BiocManager::install("KEGG.db",ask = F,update = F)
-#再不行最后用这个
+
+# 如果是存放在 github 上的包就用这个
 library(devtools)
 devtools::install_github("OHDSI/Achilles")
